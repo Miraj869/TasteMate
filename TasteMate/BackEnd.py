c09a18e0b5e0f26574152c5b551b09ccb0f56eac
Sahaj-26 <sahaj.kharidia@gmail.com>
2023-04-24 07:36:04 +0530

    created menu call

--- TasteMate/BackEnd.py
+++ TasteMate/BackEnd.py
@@ -8,77 +8,6 @@ from TasteMate import db
 from TasteMate.Models import *
 
 
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TasteMateDB.db'  # SQLite URI
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-# Define your SQL schema
-# class Business(db.Model):
-#     business_id = db.Column(db.String(25), primary_key=True)
-#     name = db.Column(db.String(255))
-#     address = db.Column(db.String(255))
-#     city = db.Column(db.String(255))
-#     state = db.Column(db.String(255))
-#     postal_code = db.Column(db.String(255))
-#     latitude = db.Column(db.Float)
-#     longitude = db.Column(db.Float)
-#     stars = db.Column(db.Float)
-#     categories = db.Column(db.String(255))
-#     hours = db.Column(db.String(255))
-#     review_count = db.Column(db.Integer)
-
-#     def __init__(self, business_id, name,address,city,state,postal_code,latitude,longitude,stars,categories,hours,review_count):
-#         self.business_id = business_id
-#         self.name = name
-#         self.address = address
-#         self.city = city
-#         self.state = state
-#         self.postal_code = postal_code
-#         self.latitude = latitude
-#         self.longitude = longitude
-#         self.stars = stars
-#         self.categories = categories
-#         self.hours = hours
-#         self.review_count = review_count
-
-
-#     def __repr__(self):
-#         return f"<Business {self.id}>"
-    
-# class Users(db.Model):
-#     user_id = db.Column(db.String(25), primary_key=True)
-#     username = db.Column(db.String(50), nullable=False)
-#     password = db.Column(db.String(20), nullable=False)
-#     # reviews = db.relationship('Review', backref='user')
-#     def __init__(self, user_id, username, password):
-#         self.user_id = user_id
-#         self.username = username
-#         self.password = password
-#     def __repr__(self):
-#         return f"<User {self.id}>"
-
-# class Reviews(db.Model):
-#     review_id = db.Column(db.String(25), primary_key=True)
-#     user_id = db.Column(db.String(25), db.ForeignKey('Users.user_id'))
-#     business_id = db.Column(db.String(25), db.ForeignKey('Business.business_id'))
-#     stars = db.Column(db.Integer,nullable=False)
-#     text = db.Column(db.Text)
-#     date = db.Column(db.DateTime)
-
-#     def __init__(self, review_id, user_id, business_id, stars, text, date):
-#         self.review_id = review_id
-#         self.user_id = user_id
-#         self.business_id = business_id
-#         self.stars = stars
-#         self.text = text
-#         self.date = date
-
-#     def __repr__(self):
-#         return f"<Review {self.id}>"
-    
-
-
 def add_review(user_id, business_id, stars, text, date):
     rev_id = ''.join(random.choices(string.ascii_uppercase +string.digits, k=20))
 
@@ -101,25 +30,6 @@ def delete_review(review_id):
     db.session.delete(rev)
     db.session.commit()
 
-
-# def validate_user(user_id,password):
-#     print("Inside Function")
-#     user = Users.query.filter_by(user_id = user_id).first()
-#     print(user)
-#     if user and user.password == password:
-#         return True
-#     else:
-#         return False
-
-# def add_user(user_id,username,password):
-#     user = Users.query.filter_by(user_id = user_id).first()
-#     if user:
-#         return False
-#     else:
-#         new_user = Users(user_id,username,password)
-#         db.session.add(new_user)
-#         db.session.commit()
-
 def openclose(business_id):
     days = {0:'Monday',1:'Tuesday',2:'Wednesday',3:'Thursday',4:'Friday',5:'Saturday',6:'Sunday'}
     bus = Business.query.filter_by(business_id = business_id).first()
@@ -127,9 +37,6 @@ def openclose(business_id):
     if bus.hours is None or len(bus.hours) < 5:
         return "Data Not Available"
     today = days[datetime.now().weekday()]
-    # print('here')
-    # print(bus.hours.replace("'","\""))
-    # print('here')
     try:
         timing = json.loads(bus.hours.replace("'","\""))
     except:
@@ -151,11 +58,6 @@ def get_dist(curlat,curlon,buslat,buslon):
 
 
 def search_buss(name=None,catlist=[], showopen = False , dist = 20,filterdist = False):
-    # print(f"name: {name}")
-    # print(f"Catlist: {catlist}")
-    # print(f"Open: {showopen}")
-    # print(dist)
-    # print(filterdist)
     results = Business.query
     if name:
         results = results.filter(Business.name.ilike(f"%{name}%"))
@@ -170,7 +72,7 @@ def search_buss(name=None,catlist=[], showopen = False , dist = 20,filterdist = 
     if showopen:
         results = [r for r in results if openclose(r.business_id) == "Open"]
     
-    results = sorted(results, key=lambda x: x.stars, reverse=True)
+    results = sorted(results, key=lambda x: x.review_count, reverse=True)
     return results
 
 
--- TasteMate/Routes.py
+++ TasteMate/Routes.py
@@ -21,24 +21,10 @@ from flask_login import login_required, current_user, login_user, logout_user
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    # isloggedIn = False
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user and user.password == form.password.data:
-    #         session['user_id'] = user.id
-    #         if form.remember_me.data:
-    #             session.permanent = True
-    #             isloggedIn = True
-    #         return redirect(url_for('home', current_user=current_user))
-    #     else:
-    #         flash('Invalid username or password', 'danger')
     if form.validate_on_submit():
         user_id = request.form['user_id']
-        # print(userid)
         password = request.form['password']
-        # print(userid)
-        # print(password)
-        # if validate_user(userid, password):
+
         user = Users.query.filter_by(user_id=user_id).first()
         if user and user.password == password:
             # flash('Congratulations! You have successfully signed in')
@@ -50,11 +36,11 @@ def login():
             return redirect('/')
     return render_template('login.html', title='Log In', form=form, current_user=current_user)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # user = Users(username=form.username.data, password=form.password.data)
         user_id = request.form['user_id']
         user = Users.query.filter_by(user_id = user_id).first()
         print(user)
@@ -79,20 +65,20 @@ def register():
             return redirect(url_for('login'))
     return render_template('register.html', title='Sign Up', form=form, current_user=current_user)
 
+
 @app.route('/home/<user>')
 @login_required
 # @oidc.accept_token(True)
 def home(user=None):
     # print(user)
     if user:
-        # print(user.user_id)
         businesses = Business.query.all()
         random_businesses = random.sample(businesses, k=4)
-        # user = Users.query.filter_by(id=session['user_id']).first()
         user1 = Users.query.filter_by(user_id=user).first()
         return render_template('home.html', title='Home', user=user1, random_businesses=random_businesses, current_user=current_user)
     else:
         return redirect(url_for('login', current_user=current_user))
+
 
 @app.route('/logout')
 @login_required
@@ -100,6 +86,7 @@ def logout():
     logout_user()
     session.pop('user_id', None)
     session.permanent = False
+    
     return redirect(url_for('login', current_user=current_user))
 
 @app.route('/businesses')
@@ -107,8 +94,6 @@ def businesses():
     catlist = []
     with open("Categories.pkl",'rb') as f:
         catlist = pickle.load(f)
-
-    # print(catlist)
     
     # Retrieve search parameters from the URL query string
     search_query = request.args.get('search_query', '')
@@ -131,15 +116,6 @@ def businesses():
     if not catlist1:
         catlist1 = []
     
-
-    # Query the businesses based on the search parameters
-    # businesses = Business.query.filter(
-    #     Business.name.like(f'%{search_query}%'),
-    #     Business.hours.like(f'%{opening_hours}%'),
-    #     Business.categories.like(f'%{cuisine_type}%'),
-    #     # Business.attributes.like(f'%{price_range}%')
-    # ).all()
-
     # Get the current page from the request arguments or default to 1
     page = request.args.get('page', 1, type=int)
 
@@ -149,43 +125,35 @@ def businesses():
     start_index = (page - 1) * per_page
     end_index = start_index + per_page
 
-    # if catlist1:
-    #     businesses = search_buss(name=search_query, catlist=catlist1, showopen=showopen, dist=distance, filterdist=filterdistance)
-    # else:
-    #     businesses = search_buss(name=search_query, catlist=[], showopen=showopen, dist=distance, filterdist=filterdistance)
     businesses = search_buss(name=search_query, catlist=catlist1, showopen=showopen, dist=distance, filterdist=filterdistance)
-    print(len(businesses))
+    
     return render_template('businesses.html', businesses=businesses, catlist=catlist, page=page, per_page=per_page, end_index=end_index, current_user=current_user)
-    # return render_template('businesses.html', businesses=businesses, catlist=catlist, page=page, per_page=per_page, current_user=current_user)
     
 
 @app.route('/businesses/<string:business_id>')
 def business_detail(business_id, page=1):
     business = Business.query.get_or_404(business_id)
     reviews = Reviews.query.filter_by(business_id=business_id).all()
-
+    usernames = []
     # Sort reviews by date in descending order
     reviews = sorted(reviews, key=lambda r: r.date, reverse=True)
-
+    for rev in reviews:
+        us = Users.query.filter_by(user_id=rev.user_id).first()
+        usernames.append(us.username)
     page = request.args.get('page', 1, type=int)
     per_page = 10
     start_index = (page - 1) * per_page
     end_index = start_index + per_page
-    # reviews = business.reviews.order_by(Review.date.desc()).slice(start_index, end_index)
 
-    return render_template('business_detail.html', business=business, reviews=reviews, page=page, per_page=per_page, end_index=end_index)
+    return render_template('business_detail.html', business=business, reviews=reviews, usernames=usernames, page=page, per_page=per_page, end_index=end_index)
 
 @app.route('/businesses/<string:business_id>/reviews/<string:review_id>', methods=['POST'])
 def insert_delete_review(business_id, review_id='None'):
-    # user_id = session.get('user_id')
-    # if not user_id:
-    #     return redirect(url_for('login', current_user=current_user))
 
     if not current_user.is_authenticated:
         return redirect(url_for('login', current_user=current_user))
 
-    # business = Business.query.get_or_404(business_id)
-    # print(review_id)
+
     if review_id == 'None':
         stars = request.form['stars1']
         review_text = request.form['review']
@@ -198,23 +166,23 @@ def insert_delete_review(business_id, review_id='None'):
     return redirect(url_for('business_detail', business_id=business_id, current_user=current_user))
 
 
-@app.route('/get_menu/<string:business_name>/<string:business_state>', methods=['POST'])
-def handle_menu_request(business_name, business_state):
-    # Get the restaurant ID from the request parameters
-    # restaurant_id = request.args.get('restaurant_id')
+@app.route('/get_menu/<string:business_name>/<string:business_state>/<string:current_section>', methods=['GET', 'POST'])
+def handle_menu_request(business_name, business_state, current_section):
     try:
         menu = get_menu(business_name, business_state)
+        if current_section == business_state:
+            current_section = request.args.get('section', list(menu.keys())[0], type=str)
+        section_items = menu[current_section]
+        # print(current_section)
+        # print(section_items)
+        # print(len(section_items))
+        if len(section_items) >= 12:
+            section_items = section_items[:12]
+        # print(current_section)
+        # print(section_items)
     except:
         menu = None
-    print(menu)
-
-    page = request.args.get('page', 1, type=int)
-
-    # Set the number of businesses to display per page
-    # per_page = 10
-
-    # start_index = (page - 1) * per_page
-    # end_index = start_index + per_page
-
-    # Render the menu template with the menu items
-    return render_template('menu.html', menu=menu, page=page)
+        current_section = ""
+        section_items = []
+    
+    return render_template('menu1.html', business_name=business_name, business_state=business_state, menu=menu, current_section=current_section, section_items=section_items)
